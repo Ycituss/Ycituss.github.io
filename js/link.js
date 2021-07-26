6c84dd6c3734d92ea47754508d1b7bfc3b9e47cb
Ycitus <7372895+ycycycc123@user.noreply.gitee.com>
2021-07-26 17:46:52 +0800

    remove game file

--- js/link.js
+++ js/link.js
@@ -4,8 +4,8 @@ function bilibili(){window.open("https://space.bilibili.com/148142151")}
 function file(){window.open("file/myapp.html" )}
 function game(){window.open("game/index.html")}
 function other(){window.open("other/index.html")}
-function app0(){window.open("/file//file/myapp/Let`s go fishing.apk")}
+function app0(){window.open("/file/file/myapp/Let`s go fishing.apk")}
 function app1(){window.open("/file/file/myapp/生成器合集.apk")}
-function mc0(){window.open("file/mc/1.15.2MCDR服务端(fabric).rar")}
-function mc1(){window.open("file/mc/1.16.1MCDR服务端(fabric).rar")}
-function mc2(){window.open("file/mc/1.16.1(fabric)服务端.rar")}
+function mc0(){window.open("/file/file/mc/1.15.2MCDR服务端(fabric).rar")}
+function mc1(){window.open("/file/file/mc/1.16.1MCDR服务端(fabric).rar")}
+function mc2(){window.open("/file/file/mc/1.16.1(fabric)服务端.rar")}
