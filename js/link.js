b831e64e2f15ef5973257aeaf242e99c7950ef03
Ycitus <7372895+ycycycc123@user.noreply.gitee.com>
2021-07-26 17:14:33 +0800

    link改为在新标签页打开

--- js/link.js
+++ js/link.js
@@ -1,11 +1,11 @@
 function error(){alert("下载功能维护中，请联系开发者");}
-function csdn(){window.location.href="https://me.csdn.net/qq_43106321"}
-function bilibili(){window.location.href="https://space.bilibili.com/148142151"}
-function file(){window.location.href="file/myapp.html" }
-function game(){window.location.href="game/index.html"}
-function other(){window.location.href="other/index.html"}
-function app0(){window.location.href="/file//file/myapp/Let`s go fishing.apk"}
-function app1(){window.location.href="/file/file/myapp/生成器合集.apk"}
-function mc0(){window.location.href="file/mc/1.15.2MCDR服务端(fabric).rar"}
-function mc1(){window.location.href="file/mc/1.16.1MCDR服务端(fabric).rar"}
-function mc2(){window.location.href="file/mc/1.16.1(fabric)服务端.rar"}
+function csdn(){window.open("https://me.csdn.net/qq_43106321")}
+function bilibili(){window.open("https://space.bilibili.com/148142151")}
+function file(){window.open("file/myapp.html" )}
+function game(){window.open("game/index.html")}
+function other(){window.open("other/index.html")}
+function app0(){window.open("/file//file/myapp/Let`s go fishing.apk")}
+function app1(){window.open("/file/file/myapp/生成器合集.apk")}
+function mc0(){window.open("file/mc/1.15.2MCDR服务端(fabric).rar")}
+function mc1(){window.open("file/mc/1.16.1MCDR服务端(fabric).rar")}
+function mc2(){window.open("file/mc/1.16.1(fabric)服务端.rar")}
